bd371bc841343dad620f06816e25836108946f7d
sengheang <sengheang908@gmail.com>
2026-02-24 14:37:06 +0700

    Create system to enroll student use with PHP

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$dbname = "school_db";
-$user = "root";
-$pass = ""; // if you have password, put here
+$host = "sql206.infinityfree.com";
+$dbname = "if0_41232377_school_db";
+$user = "if0_41232377";
+$pass = "94ac4PTHKOyCUM"; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
